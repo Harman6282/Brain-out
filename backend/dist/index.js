2dafb9114f006dc0d2316e2a0a6bc593a4733c63
Harman6282 <harmans6282@gmail.com>
2024-12-22 14:39:43 +0530

    basic routes and sign up/in

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -12,18 +12,48 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const db_1 = __importDefault(require("./db"));
 const user_model_1 = require("./models/user.model");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.post("/api/v1/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    //? zod validation and hash the password
     const { username, password } = req.body;
-    const newUser = yield user_model_1.UserModel.create({
-        username: username,
-        password: password,
-    });
-    res.status(200).json({ message: "Signed up successfully" });
+    try {
+        const newUser = yield user_model_1.UserModel.create({
+            username: username,
+            password: password,
+        });
+        res
+            .status(200)
+            .json({ message: "Signed up successfully", newUser: newUser });
+    }
+    catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+    }
+}));
+app.post("/api/v1/signin", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    //? zod validation 
+    const { username, password } = req.body;
+    try {
+        const getUser = yield user_model_1.UserModel.findOne({
+            username: username,
+            password: password,
+        });
+        if (getUser) {
+            const token = jsonwebtoken_1.default.sign({
+                id: getUser._id
+            }, "mostimportantjwtsecret");
+            console.log(token);
+            res.status(200).json({ message: "signed in successfully", getUser, token: token });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: "Something went wrong in signin" });
+    }
 }));
 app.delete("/api/v1/content", (req, res) => { });
 app.post("/api/v1/brain/share", (req, res) => { });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
@@ -7,17 +8,58 @@ const app = express();
 
 app.use(express.json());
 
+
 app.post("/api/v1/signup", async (req, res) => {
-    //? zod validation and hash the password 
+  //? zod validation and hash the password
   const { username, password } = req.body;
 
-  const newUser = await UserModel.create({
-    username: username,
-    password: password,
-  });
+  try {
+    const newUser = await UserModel.create({
+      username: username,
+      password: password,
+    });
 
-  res.status(200).json({ message: "Signed up successfully" });
+
+    res
+      .status(200)
+      .json({ message: "Signed up successfully", newUser: newUser });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
+
+app.post("/api/v1/signin", async (req, res) => {
+  //? zod validation 
+  const { username, password } = req.body;
+try {
+
+  if(!username || !password){
+     res.status(400).json({ message: "Please provide username and password" }); 
+  }
+  
+    const getUser = await UserModel.findOne({
+      username: username,
+      password: password,
+    });
+    
+    if(!getUser){
+       res.status(404).json({ message: "User not found" }); 
+    }
+  
+  
+    if (getUser) {
+      const token = jwt.sign({
+        id: getUser._id
+      }, "mostimportantjwtsecret" );
+      console.log(token)
+      res.status(200).json({ message: "signed in successfully", getUser , token: token});
+    }
+  
+} catch (error) {
+  res.status(500).json({ message: "Something went wrong in signin" });
+}
+});
+
 app.delete("/api/v1/content", (req, res) => {});
 app.post("/api/v1/brain/share", (req, res) => {});
 app.get("/api/v1/brain/:shareLink", (req, res) => {});
