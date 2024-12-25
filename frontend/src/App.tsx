2e0890aa97ccccc9cfb5173b1a77ed17f7749373
Harman6282 <harmans6282@gmail.com>
2024-12-25 14:43:27 +0530

    add button ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Button } from "./components/Button";
+import { PlusIcon } from "./icons/PlusIcon";
+import { ShareIcon } from "./icons/ShareIcon";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button
+        startIcon={<ShareIcon size="sm" />}
+        variant="primary"
+        size="sm"
+        text="small"
+        onClick={() => {}}
+      />
+      <Button
+        startIcon={<PlusIcon size="md" />}
+        variant="secondary"
+        size="md"
+        text="medium"
+        onClick={() => {}}
+      />
+      <Button
+        startIcon={<ShareIcon size="lg" />}
+        variant="secondary"
+        size="lg"
+        text="large"
+        onClick={() => {}}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
